b70c1e565a10d6ed339f5eecbdf6cbd8d426eaf2
Ayush <nairayush45@gmail.com>
2025-01-27 23:55:36 +0530

    frontend commmit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import NavbarComponent from './Components/nav.jsx'
 import FrontPage from './Components/FrontPage.jsx'
+import WorkoutComponent from './Components/WorkoutComponent.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 function App() {
- 
 
   return (
     <>
---
+++ frontend/src/Components/WorkoutComponent.jsx
@@ -0,0 +1,11 @@
+
+
+function WorkoutComponent() {
+    return(
+        <div>
+            <p>HELLO</p>
+        </div>
+    )
+}
+
+export default WorkoutComponent;
